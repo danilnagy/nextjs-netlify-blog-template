09fdcddc5faef6295f7f09503bceda3fc32a15ff
Danil Nagy <4085524+danilnagy@users.noreply.github.com>
2023-01-12 14:24:38 -0500

    Update identity-signup.js

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -6,41 +6,41 @@ exports.handler = async (event) => {
 
   console.log(user);
 
-  // create a new customer in Stripe
-  const customer = await stripe.customers.create({
-    email: user.email,
-    name: user.user_metadata.full_name,
-  });
+  // // create a new customer in Stripe
+  // const customer = await stripe.customers.create({
+  //   email: user.email,
+  //   name: user.user_metadata.full_name,
+  // });
 
-  // subscribe the new customer to the free plan
-  await stripe.subscriptions.create({
-    customer: customer.id,
-    items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
-  });
+  // // subscribe the new customer to the free plan
+  // await stripe.subscriptions.create({
+  //   customer: customer.id,
+  //   items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
+  // });
 
-  // store the Netlify and Stripe IDs in Fauna
-  const result = await faunaFetch({
-    query: `
-      mutation ($netlifyID: ID!, $stripeID: ID!) {
-        createUser(data: { netlifyID: $netlifyID, stripeID: $stripeID }) {
-          netlifyID
-          stripeID
-        }
-      }
-    `,
-    variables: {
-      netlifyID: user.id,
-      stripeID: customer.id,
-    },
-  });
-  console.log(result);
+  // // store the Netlify and Stripe IDs in Fauna
+  // const result = await faunaFetch({
+  //   query: `
+  //     mutation ($netlifyID: ID!, $stripeID: ID!) {
+  //       createUser(data: { netlifyID: $netlifyID, stripeID: $stripeID }) {
+  //         netlifyID
+  //         stripeID
+  //       }
+  //     }
+  //   `,
+  //   variables: {
+  //     netlifyID: user.id,
+  //     stripeID: customer.id,
+  //   },
+  // });
+  // console.log(result);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      app_metadata: {
-        roles: ["free"],
-      },
-    }),
-  };
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     app_metadata: {
+  //       roles: ["free"],
+  //     },
+  //   }),
+  // };
 };
