c83c949af933e3e0780f83910b12ae1ed66b2096
Danil Nagy <4085524+danilnagy@users.noreply.github.com>
2023-03-24 10:13:00 -0400

    next upgrade and video test

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -8,6 +8,7 @@ import config from "../../lib/config";
 import { countPosts, listPostContent, PostContent } from "../../lib/posts";
 import { listTags, TagContent } from "../../lib/tags";
 import Head from "next/head";
+import Script from "next/script";
 
 type Props = {
   posts: PostContent[];
@@ -22,6 +23,25 @@ export default function Index({ posts, tags, pagination }: Props) {
   const title = "All posts";
   return (
     <Layout>
+      <Script strategy="afterInteractive" src="https://assets.swarmcdn.com/cross/swarmdetect.js"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var swarmoptions = {
+                swarmcdnkey: "68f9693f-4651-4ab6-9a03-2760ae3024f3",
+                iframeReplacement: "iframe",
+                autoreplace: {
+                    youtube: true
+                },
+                theme: {
+                    button: "circle",
+                    primaryColor: "#328ac6"
+                }
+            };`,
+          }}
+      />
       <BasicMeta url={url} title={title} />
       <OpenGraphMeta url={url} title={title} />
       <TwitterCardMeta url={url} title={title} />
